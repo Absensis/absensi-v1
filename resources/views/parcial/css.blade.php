3783b1586880a5c0afe7ea84599934163dee6319
Muhammad Farras Jibran <farasjibran@gmail.com>
2020-10-24 01:00:24 +0700

    update template

---
+++ resources/views/parcial/css.blade.php
@@ -0,0 +1,24 @@
+<!-- Animate.css -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.css">
+<!-- Icomoon Icon Fonts-->
+<!-- <link rel="stylesheet" href="{{ asset('css/icomoon.css')}}"> -->
+<!-- Bootstrap  -->
+<link rel="stylesheet" href="{{ asset('css/bootstrap.css')}}">
+
+<!-- Magnific Popup -->
+<link rel="stylesheet" href="{{ asset('css/magnific-popup.css')}}">
+
+<!-- Owl Carousel -->
+<link rel="stylesheet" href="{{ asset('css/owl.carousel.min.css')}}">
+<link rel="stylesheet" href="{{ asset('css/owl.theme.default.min.css')}}">
+
+<!-- Theme style  -->
+<link rel="stylesheet" href="{{ asset('css/style.css')}}">
+
+<!-- Modernizr JS -->
+<script src="{{ asset('js/modernizr-2.6.2.min.js')}}"></script>
+
+<link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Nunito:200,300,400" rel="stylesheet">
+
+<link href="{{ asset('vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
---
+++ resources/views/parcial/js.blade.php
@@ -0,0 +1,21 @@
+ <!-- jQuery -->
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.1/jquery.min.js"></script>
+ <!-- jQuery Easing -->
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+ <!-- Bootstrap -->
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/js/bootstrap.min.js"></script>
+ <!-- Waypoints -->
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js"></script>
+ <!-- Stellar Parallax -->
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/stellar.js/0.5.0/jquery.stellar.min.js"></script>
+ <!-- YTPlayer -->
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mb.YTPlayer/3.3.4/jquery.mb.YTPlayer.min.js"></script>
+ <!-- Owl carousel -->
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.min.js"></script>
+ <!-- Magnific Popup -->
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js"></script>
+ <script src="{{ asset('js/magnific-popup-options.js')}}"></script>
+ <!-- Counters -->
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-countto/1.2.0/jquery.countTo.min.js"></script>
+ <!-- Main -->
+ <script src="{{ asset('js/main.js')}}"></script>
