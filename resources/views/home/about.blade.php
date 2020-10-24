084b63aa51bfb0138952122341c91e8a804fe365
Muhammad Farras Jibran <farasjibran@gmail.com>
2020-10-24 13:25:12 +0700

    Update about.blade.php

--- resources/views/home/about.blade.php
+++ resources/views/home/about.blade.php
@@ -28,7 +28,7 @@
                 </div>
             </div>
             <div class="col-md-6 animate-box">
-                <h2>About unapp</h2>
+                <h2>About {{ config('app.name', 'Laravel') }}</h2>
                 <p>The Big Oxmox advised her not to do so, because there were thousands of bad Commas, wild Question Marks and devious Semikoli, but the Little Blind Text didn’t listen. She packed her seven versalia, put her initial into the belt and made herself on the way.</p>
                 <p>When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrove, the headline of Alphabet Village and the subline of her own road, the Line Lane. Pityful a rethoric question ran over her cheek, then she continued her way.</p>
             </div>
